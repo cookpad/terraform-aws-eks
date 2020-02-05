6d2948ef6ebdfdad75fb959501d24a7b305c17fd
Ed Robinson <edward-robinson@cookpad.com>
2020-02-05 14:25:53 +0000

    Adds test_structure to make local debugging / hacking much simpler
    * https://terratest.gruntwork.io/docs/testing-best-practices/iterating-locally-using-test-stages/
    * https://github.com/gruntwork-io/terratest/blob/master/test/terraform_packer_example_test.go

--- test/cluster_example_test.go
+++ test/cluster_example_test.go
@@ -2,35 +2,56 @@ package test
 
 import (
 	"fmt"
-	"github.com/gruntwork-io/terratest/modules/k8s"
-	"github.com/gruntwork-io/terratest/modules/random"
-	"github.com/gruntwork-io/terratest/modules/terraform"
 	"io/ioutil"
 	"os"
 	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 )
 
-func TestTerraformAwsHelloWorldExample(t *testing.T) {
+func TestTerraformAwsEksExample(t *testing.T) {
 	t.Parallel()
 
+	workingDir := "../examples/cluster"
+
+	test_structure.RunTestStage(t, "deploy_terraform", func() {
+		deployTerraform(t, workingDir)
+	})
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created.
+	defer test_structure.RunTestStage(t, "cleanup_terraform", func() {
+		cleanupTerraform(t, workingDir)
+	})
+
+	test_structure.RunTestStage(t, "validate_cluster", func() {
+		validateCluster(t, workingDir)
+	})
+}
+
+func deployTerraform(t *testing.T, workingDir string) {
 	uniqueId := random.UniqueId()
 	clusterName := fmt.Sprintf("testing-%s", uniqueId)
 
 	terraformOptions := &terraform.Options{
 		// The path to where our Terraform code is located
-		TerraformDir: "../examples/cluster",
+		TerraformDir: workingDir,
 		Vars: map[string]interface{}{
 			"cluster_name": clusterName,
 		},
 	}
 
-	// At the end of the test, run `terraform destroy` to clean up any resources that were created.
-	defer terraform.Destroy(t, terraformOptions)
+	// Save the Terraform Options struct, so future test stages can use it
+	test_structure.SaveTerraformOptions(t, workingDir, terraformOptions)
 
 	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
+}
 
-	// Write the kubeconfig file
+func validateCluster(t *testing.T, workingDir string) {
+	terraformOptions := test_structure.LoadTerraformOptions(t, workingDir)
 	kubeconfig, err := writeKubeconfig(terraform.Output(t, terraformOptions, "kubeconfig"))
 	if err != nil {
 		t.Error("Error writing kubeconfig file:", err)
@@ -41,6 +62,12 @@ func TestTerraformAwsHelloWorldExample(t *testing.T) {
 
 	// Do a thing with the API to check it works
 	k8s.GetServiceAccount(t, k8sOptions, "default")
+
+}
+
+func cleanupTerraform(t *testing.T, workingDir string) {
+	terraformOptions := test_structure.LoadTerraformOptions(t, workingDir)
+	terraform.Destroy(t, terraformOptions)
 }
 
 func writeKubeconfig(config string) (string, error) {
