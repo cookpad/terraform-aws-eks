3f7fd957142f491530d7e9624ecf2f338f09ca9d
Ed Robinson <edward-robinson@cookpad.com>
2020-03-09 16:59:41 +0000

    Shorten name prefix

--- examples/cluster/test_iam_role.tf
+++ examples/cluster/test_iam_role.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_iam_role" "test_role" {
-  name_prefix        = "TerraformAWSEKS-${var.cluster_name}"
+  name_prefix        = "TerraformAWSEKS"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
