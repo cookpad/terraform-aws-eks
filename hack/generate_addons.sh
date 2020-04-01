760fa428c0ddf3d525c55dc89830dac67b72f548
Ed Robinson <edward-robinson@cookpad.com>
2020-04-01 10:09:57 +0100

    Update addons

--- hack/generate_addons.sh
+++ hack/generate_addons.sh
@@ -13,7 +13,7 @@ helm_template() {
   helm template --no-hooks --namespace=kube-system --version $3 -f $ADDONS_DIR/helm/$2.yaml $2 $1/$2 | grep -v Helm > $ADDONS_DIR/$2.yaml
 }
 
-helm_template eks aws-node-termination-handler 0.5.1
-helm_template stable cluster-autoscaler 6.6.1
-helm_template stable metrics-server 2.10.0
-helm_template stable prometheus-node-exporter 1.9.0
+helm_template eks aws-node-termination-handler 0.7.3
+helm_template stable cluster-autoscaler 7.2.0
+helm_template stable metrics-server 2.10.1
+helm_template stable prometheus-node-exporter 1.9.1
