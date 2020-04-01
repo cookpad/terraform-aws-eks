af071c50b2b081c9d7448e6e9e5dd9799e90f0c0
Ed Robinson <edward-robinson@cookpad.com>
2020-04-01 17:05:34 +0100

    Adds cleanup script [skip ci]

--- hack/cleanup.sh
+++ hack/cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export AWS_ROLE_ARN="arn:aws:iam::214219211678:role/TerraformAWSEKSTests"
-export SKIP_cleanup_terraform=true
+export SKIP_deploy_terraform=true
+export SKIP_validate=true
 
 .github/actions/terratest/entrypoint.sh -run "${@:-TestTerraformAwsEksCluster}"
