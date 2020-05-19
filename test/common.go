14216e949f163bd515f1363e51a22d1110fd4c65
Ed Robinson <edward-robinson@cookpad.com>
2020-05-19 16:50:04 +0100

    Use the coredns manifest from https://docs.aws.amazon.com/eks/latest/userguide/coredns.html
    Also allow the service ip for cluster dns to be overidden
    
    Adds the metrics port to the service to allow prometheus to scrape
    metrics!

--- test/common.go
+++ test/common.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/retry"
 	"github.com/gruntwork-io/terratest/modules/shell"
@@ -240,4 +241,43 @@ func waitForNodes(t *testing.T, kubectlOptions *k8s.KubectlOptions, numNodes int
 
 	// Wait for the nodes to be ready
 	k8s.WaitUntilAllNodesReady(t, kubectlOptions, maxRetries, sleepBetweenRetries)
+}
+
+func WaitUntilPodsSucceededE(t *testing.T, options *k8s.KubectlOptions, filters metav1.ListOptions, desiredCount, retries int, sleepBetweenRetries time.Duration) error {
+	statusMsg := "Wait for pods to Succeed."
+	message, err := retry.DoWithRetryE(
+		t,
+		statusMsg,
+		retries,
+		sleepBetweenRetries,
+		func() (string, error) {
+			pods, err := k8s.ListPodsE(t, options, filters)
+			if err != nil {
+				return "", err
+			}
+			if len(pods) != desiredCount {
+				return "", k8s.DesiredNumberOfPodsNotCreated{Filter: filters, DesiredCount: desiredCount}
+			}
+			for _, pod := range pods {
+				pod, err := k8s.GetPodE(t, options, pod.Name)
+				if err != nil {
+					return "", err
+				}
+				if pod.Status.Phase != corev1.PodSucceeded {
+					return "", k8s.NewPodNotAvailableError(pod)
+				}
+			}
+			return "Pods have now Succeeded", nil
+		},
+	)
+	if err != nil {
+		logger.Logf(t, "Timedout waiting for the desired number of Pods to Succeed: %s", err)
+		return err
+	}
+	logger.Logf(t, message)
+	return nil
+}
+
+func WaitUntilPodsSucceeded(t *testing.T, options *k8s.KubectlOptions, filters metav1.ListOptions, desiredCount, retries int, sleepBetweenRetries time.Duration) {
+	require.NoError(t, WaitUntilPodsSucceededE(t, options, filters, desiredCount, retries, sleepBetweenRetries))
 }
