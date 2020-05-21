008a814dc5c850e43321594f528e7d3d4e7b913a
ettiee <ettie-eyre@cookpad.com>
2020-05-21 16:14:03 +0100

    fix state bug in 1.15 upgrade

--- hack/update_1_15.sh
+++ hack/update_1_15.sh
@@ -5,4 +5,3 @@ set -xeuo pipefail
 terraform state mv module.$1.aws_security_group.control_plane{,[0]}
 terraform state mv module.$1.aws_security_group.node{,[0]}
 terraform state mv module.$1.aws_security_group_rule.node_ingress_self{,[0]}
-terraform state mv module.$1.aws_security_group_rule.node_ingress_cluster{,[0]}
