bfeb147c75dedc1b0a5e896f8bbef7937fecd695
Ed Robinson <edward-robinson@cookpad.com>
2020-09-01 10:47:13 +0100

    Make output a bit more readable

--- hack/roll_nodes.sh
+++ hack/roll_nodes.sh
@@ -6,7 +6,7 @@
 #
 # This script depends on a correctly configured kubectl, jq and aws cli
 
-set -xeuo pipefail
+set -euo pipefail
 
 # If you need some extra aws cli flags in your environment e.g. --profile=foo add
 # them here!
@@ -35,22 +35,33 @@ nodes_rolled() {
 }
 
 wait_for_pods() {
+  printf "waiting for pods to be rescheduled "
   while [ $(kubectl get pods --all-namespaces -o json | jq '.items | map(select(.status.phase=="Running" | not)) | map(select(.status.phase=="Succeeded" | not)) | length') -gt 0 ]
   do
+    printf "."
     sleep 10
   done
+  echo
 }
 
 detach() {
+  instance=$(instance_id $1)
+  group=$(autoscaling_group_name $instance)
+  [ -z "$group" ] && echo "node $1 is not in an asg" && return 0
+
+  echo "detaching node $1 from it's asg $group"
   rolled=$(nodes_rolled)
   aws $AWS_EXTRA_ARGS autoscaling detach-instances \
-    --instance-ids $1 \
-    --auto-scaling-group-name $(autoscaling_group_name $1) \
+    --instance-ids $instance \
+    --auto-scaling-group-name $group \
     --no-should-decrement-desired-capacity
+  printf "waiting for a new node to start "
   while [ $(nodes_rolled) -le $rolled ]
   do
+    printf "."
     sleep 10
   done
+  echo
 }
 
 for NODE in $NODES_TO_ROLL
@@ -60,9 +71,8 @@ done
 
 for NODE in $NODES_TO_ROLL
 do
-  instance=$(instance_id $NODE)
-  detach $instance
+  detach $NODE
   kubectl drain --delete-local-data --ignore-daemonsets $NODE
-  aws $AWS_EXTRA_ARGS ec2 terminate-instances --instance-ids $instance
+  aws $AWS_EXTRA_ARGS ec2 terminate-instances --instance-ids $(instance_id $NODE)
   wait_for_pods
 done
