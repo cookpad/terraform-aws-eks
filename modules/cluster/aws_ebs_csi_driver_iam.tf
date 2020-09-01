e0a6d59d113eb8c663c4ecaceff7d0523d8f6435
Ed Robinson <edward-robinson@cookpad.com>
2020-09-01 16:23:18 +0100

    Update storage classes

--- modules/cluster/aws_ebs_csi_driver_iam.tf
+++ modules/cluster/aws_ebs_csi_driver_iam.tf
@@ -11,7 +11,7 @@ data "aws_iam_policy_document" "aws_ebs_csi_driver_assume_role_policy" {
     condition {
       test     = "StringEquals"
       variable = "${replace(aws_iam_openid_connect_provider.cluster_oidc.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:kube-system:cluster-autoscaler"]
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa", "system:serviceaccount:kube-system:ebs-snapshot-controller"]
     }
 
     principals {
