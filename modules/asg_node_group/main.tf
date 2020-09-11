07a72643bee205d8059dc4324afc468609485175
Ed Robinson <edward-robinson@cookpad.com>
2020-09-11 11:51:14 +0100

    Adds node region and zone topology labels to autoscaler template
    This will assist cluster autoscaler in choosing the right ASG to scale
    up when zone is important, e.g. when handling EBS volumes or zone
    (anti)affinity!

--- modules/asg_node_group/main.tf
+++ modules/asg_node_group/main.tf
@@ -35,6 +35,8 @@ data "aws_ami" "image" {
   }
 }
 
+data "aws_region" "current" {}
+
 data "template_file" "cloud_config" {
   template = file("${path.module}/cloud_config.tpl")
   vars = {
@@ -139,6 +141,36 @@ resource "aws_autoscaling_group" "nodes" {
     key                 = "Role"
     value               = "eks-node"
     propagate_at_launch = true
+  }
+
+  tag {
+    key = "k8s.io/cluster-autoscaler/node-template/label/topology.ebs.csi.aws.com"
+    value = each.key
+    propagate_at_launch = false
+  }
+
+  tag {
+    key = "k8s.io/cluster-autoscaler/node-template/label/topology.kubernetes.io/zone"
+    value = each.key
+    propagate_at_launch = false
+  }
+
+  tag {
+    key = "k8s.io/cluster-autoscaler/node-template/label/failure-domain.beta.kubernetes.io/zone"
+    value = each.key
+    propagate_at_launch = false
+  }
+
+  tag {
+    key = "k8s.io/cluster-autoscaler/node-template/label/topology.kubernetes.io/region"
+    value = data.aws_region.current.name
+    propagate_at_launch = false
+  }
+
+  tag {
+    key = "k8s.io/cluster-autoscaler/node-template/label/topology.kubernetes.io/region"
+    value = data.aws_region.current.name
+    propagate_at_launch = false
   }
 
   dynamic "tag" {
