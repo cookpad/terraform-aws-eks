202cf18428ba2bfbd56c7a6bb41dc4a0ffc29613
Ed Robinson <edward-robinson@cookpad.com>
2020-09-28 19:02:51 +0100

    Fix flaky cleanup (#146)

--- test/common.go
+++ test/common.go
@@ -1,9 +1,14 @@
 package test
 
 import (
+	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
 func deployTerraform(t *testing.T, workingDir string, vars map[string]interface{}) {
@@ -30,4 +35,69 @@ func cleanupTerraform(t *testing.T, workingDir string) {
 	terraformOptions := test_structure.LoadTerraformOptions(t, workingDir)
 	terraform.Destroy(t, terraformOptions)
 	test_structure.CleanupTestDataFolder(t, workingDir)
+}
+
+func removeSecurityGroups(t *testing.T, workingDir string) {
+	terraformOptions := test_structure.LoadTerraformOptions(t, workingDir)
+	vpcId := terraform.Output(t, terraformOptions, "vpc_id")
+	client := ec2.New(session.New(&aws.Config{Region: aws.String("us-east-1")}))
+	securityGroups(t, client, vpcId, func(sg *ec2.SecurityGroup) {
+		revokeRules(t, client, sg)
+	})
+	securityGroups(t, client, vpcId, func(sg *ec2.SecurityGroup) {
+		deleteSecurityGroup(t, client, sg)
+	})
+
+}
+
+func securityGroups(t *testing.T, client *ec2.EC2, vpcId string, function func(*ec2.SecurityGroup)) {
+	input := &ec2.DescribeSecurityGroupsInput{
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String("vpc-id"),
+				Values: []*string{
+					aws.String(vpcId),
+				},
+			},
+		},
+	}
+	err := client.DescribeSecurityGroupsPages(
+		input,
+		func(page *ec2.DescribeSecurityGroupsOutput, lastPage bool) bool {
+			for _, sg := range page.SecurityGroups {
+				function(sg)
+			}
+			return !lastPage
+		},
+	)
+	if err != nil {
+		logger.Log(t, err.Error())
+	}
+}
+
+func deleteSecurityGroup(t *testing.T, client *ec2.EC2, sg *ec2.SecurityGroup) {
+	if *sg.GroupName == "default" {
+		return
+	}
+	logger.Log(t, "Deleting security group:", *sg.GroupName, *sg.GroupId)
+	deleteInput := &ec2.DeleteSecurityGroupInput{
+		GroupId: aws.String(*sg.GroupId),
+	}
+	_, err := client.DeleteSecurityGroup(deleteInput)
+	if err != nil {
+		logger.Log(t, err.Error())
+	}
+}
+
+func revokeRules(t *testing.T, client *ec2.EC2, sg *ec2.SecurityGroup) {
+	for _, permission := range sg.IpPermissions {
+		input := &ec2.RevokeSecurityGroupIngressInput{
+			GroupId:       aws.String(*sg.GroupId),
+			IpPermissions: []*ec2.IpPermission{permission},
+		}
+		_, err := client.RevokeSecurityGroupIngress(input)
+		if err != nil {
+			logger.Log(t, err.Error())
+		}
+	}
 }
