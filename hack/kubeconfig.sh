ff6e7286d5a9c8c0827b9172b310335b53715d31
Ed Robinson <edward-robinson@cookpad.com>
2020-11-06 15:33:36 +0000

    Don't leak kubeconfig file contents into terraform log output (#167)
    * Don't leak kubeconfig file contents into terraform log output
    
    Fixes #166
    
    * fmt

--- hack/kubeconfig.sh
+++ hack/kubeconfig.sh
@@ -4,5 +4,4 @@ export AWS_ROLE_ARN="arn:aws:iam::214219211678:role/TerraformAWSEKSTests"
 source hack/assume_role.sh
 
 cd examples/cluster
-aws eks update-kubeconfig --name $(terraform output cluster_name) --role-arn $AWS_ROLE_ARN
-
+aws eks update-kubeconfig --name $(terraform output cluster_name) --role-arn $AWS_ROLE_ARN --alias=terraform-aws-eks-test
--- modules/cluster/kubectl/command.sh
+++ modules/cluster/kubectl/command.sh
@@ -1,26 +1,3 @@
-cat <<EOF > ${kubeconfig}
-apiVersion: v1
-kind: Config
-clusters:
-- name: ${cluster_name}
-  cluster:
-    certificate-authority-data: ${ca_data}
-    server: ${endpoint}
-users:
-- name: ${cluster_name}
-  user:
-    token: ${token}
-contexts:
-- name: ${cluster_name}
-  context:
-    cluster: ${cluster_name}
-    user: ${cluster_name}
-    namespace: ${namespace}
-current-context: ${cluster_name}
-EOF
-
-kubectl --kubeconfig=${kubeconfig} apply -f -<<EOF
-${manifest}
-EOF
-
-rm ${kubeconfig}
+echo "$KUBECONFIG" > ${kubeconfig_path}
+echo "$MANIFEST" | kubectl --kubeconfig=${kubeconfig_path} apply -f -
+rm ${kubeconfig_path}
