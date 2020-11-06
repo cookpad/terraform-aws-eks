8860d3f567252fbcbb160d20b93877a5609bcd72
Ed Robinson <edward-robinson@cookpad.com>
2020-11-06 13:13:30 +0000

    Don't leak kubeconfig file contents into terraform log output
    Fixes #166

--- modules/cluster/kubectl/command.sh
+++ modules/cluster/kubectl/command.sh
@@ -1,36 +1,12 @@
-cat <<EOF > ${kubeconfig}
-apiVersion: v1
-kind: Config
-clusters:
-- name: ${cluster_name}
-  cluster:
-    certificate-authority-data: ${ca_data}
-    server: ${endpoint}
-users:
-- name: ${cluster_name}
-  user:
-    token: ${token}
-contexts:
-- name: ${cluster_name}
-  context:
-    cluster: ${cluster_name}
-    user: ${cluster_name}
-    namespace: ${namespace}
-current-context: ${cluster_name}
-EOF
-
-MANIFEST="$(cat <<EOF
-${manifest}
-EOF
-)"
+echo "$KUBECONFIG" > ${kubeconfig_path}
 
 %{ for r in replace ~}
-echo "$MANIFEST" | kubectl --kubeconfig=${kubeconfig} apply -f - || echo "$MANIFEST" | kubectl --kubeconfig=${kubeconfig} replace --force --save-config -f -
+echo "$MANIFEST" | kubectl --kubeconfig=${kubeconfig_path} apply -f - || echo "$MANIFEST" | kubectl --kubeconfig=${kubeconfig_path} replace --force --save-config -f -
 %{ endfor ~}
 
 %{ for a in apply ~}
-echo "$MANIFEST" | kubectl --kubeconfig=${kubeconfig} apply -f -
+echo "$MANIFEST" | kubectl --kubeconfig=${kubeconfig_path} apply -f -
 %{ endfor ~}
 
 
-rm ${kubeconfig}
+rm ${kubeconfig_path}
