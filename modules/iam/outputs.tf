1a6aecb62d6e15d9d70fe44ea53e307165da026e
Takayuki WATANABE <takanabe.w@gmail.com>
2021-03-10 17:21:46 +0900

    Pass ARNs of node/service IAM roles instead of name
    See https://github.com/cookpad/terraform-aws-eks/pull/201

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,8 +1,10 @@
 output "config" {
   value = {
-    service_role = aws_iam_role.eks_service_role.name
-    node_role    = aws_iam_role.eks_node.name
+    service_role_arn = aws_iam_role.eks_service_role.arn
+    node_role        = aws_iam_role.eks_node.name
+    node_role_arn    = aws_iam_role.eks_node.arn
   }
+
   depends_on = [
     aws_iam_role_policy_attachment.eks_worker_node,
     aws_iam_role_policy_attachment.eks_cni,
