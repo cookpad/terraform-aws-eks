424cf1ad693f20c3aae1d280582ba98a29fbe3e0
Dan Slinky <dan-slinky@cookpad.jp>
2021-03-11 12:22:19 +0000

    remove deprecated failure-domain.beta.kubernetes.io/zone label

--- modules/asg_node_group/main.tf
+++ modules/asg_node_group/main.tf
@@ -178,12 +178,6 @@ resource "aws_autoscaling_group" "nodes" {
   }
 
   tag {
-    key                 = "k8s.io/cluster-autoscaler/node-template/label/failure-domain.beta.kubernetes.io/zone"
-    value               = each.key
-    propagate_at_launch = false
-  }
-
-  tag {
     key                 = "k8s.io/cluster-autoscaler/node-template/label/topology.kubernetes.io/region"
     value               = data.aws_region.current.name
     propagate_at_launch = false
