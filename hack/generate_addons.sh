a30541b6d590e6b45a3f24c88df7108b518934f8
ettiee <ettie-eyre@cookpad.com>
2021-06-02 14:20:49 +0100

    1.18.4 generate_addons conflicts

--- hack/generate_addons.sh
+++ hack/generate_addons.sh
@@ -23,6 +23,6 @@ helm_template nvdp nvidia-device-plugin 0.8.2
 
 curl -o $ADDONS_DIR/kustomize/overlays/metrics-server/resources/metrics-server.yaml -L https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.7/components.yaml
 kustomize_build metrics-server
-curl -o $ADDONS_DIR/kustomize/overlays/aws-ebs-csi-driver/resources/crd_snapshotter.yaml -L https://raw.githubusercontent.com/kubernetes-sigs/aws-ebs-csi-driver/v0.7.0/deploy/kubernetes/cluster/crd_snapshotter.yaml
+curl -o $ADDONS_DIR/kustomize/overlays/aws-ebs-csi-driver/resources/crd_snapshotter.yaml -L https://raw.githubusercontent.com/kubernetes-sigs/aws-ebs-csi-driver/v1.0.0/deploy/kubernetes/cluster/crd_snapshotter.yaml
 kustomize_build aws-ebs-csi-driver
 kustomize_build aws-alb-ingress-controller
