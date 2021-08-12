e78d369812efdf195add3b5f6e89f3ed26f09646
Ed Robinson <edward-robinson@cookpad.com>
2021-08-12 11:35:55 +0100

    fmt

--- modules/asg_node_group/main.tf
+++ modules/asg_node_group/main.tf
@@ -53,9 +53,9 @@ data "aws_region" "current" {}
 data "template_file" "cloud_config" {
   template = file("${path.module}/cloud_config.tpl")
   vars = {
-    cluster_name   = var.cluster_config.name
-    labels         = join(",", [for label, value in local.labels : "${label}=${value}"])
-    taints         = join(",", [for taint, value_effect in var.taints : "${taint}=${value_effect}"])
+    cluster_name = var.cluster_config.name
+    labels       = join(",", [for label, value in local.labels : "${label}=${value}"])
+    taints       = join(",", [for taint, value_effect in var.taints : "${taint}=${value_effect}"])
   }
 }
 
