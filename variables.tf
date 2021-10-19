a8132a7de176e2294c8d2fc6a291528e6126213c
Noah Berman <noah-berman@cookpad.com>
2021-10-19 14:45:31 +0100

    Allow documented minimum possible config to be used

--- variables.tf
+++ variables.tf
@@ -3,6 +3,12 @@ variable "cluster_name" {
   description = "A name for the EKS cluster, and the resources it depends on"
 }
 
+variable "cluster_names" {
+  type        = list(string)
+  default     = []
+  description = "Names of the EKS clusters deployed in the VPC."
+}
+
 variable "cidr_block" {
   type        = string
   description = "The CIDR block for the VPC that EKS will run in"
@@ -28,4 +34,3 @@ variable "envelope_encryption_enabled" {
   default     = true
   description = "Should Cluster Envelope Encryption be enabled, if changed after provisioning - forces the cluster to be recreated"
 }
-
