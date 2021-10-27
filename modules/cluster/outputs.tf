372fdecc12cc206fbe4c407d14225bceb0ae304d
Takayuki Watanabe <takanabe.w@gmail.com>
2021-10-27 23:16:45 +0900

    cluster mod: comments about regular expressions to retrieve IAM role name

--- modules/cluster/outputs.tf
+++ modules/cluster/outputs.tf
@@ -6,6 +6,8 @@ locals {
     vpc_id                = var.vpc_config.vpc_id
     private_subnet_ids    = var.vpc_config.private_subnet_ids
     node_security_group   = aws_eks_cluster.control_plane.vpc_config.0.cluster_security_group_id
+    # this regular expression comes from the following link to retrieve role names
+    # https://github.com/cookpad/terraform-aws-eks/pull/257/commits/99fae9b5dab1b2b2d2b1085a479f57d45e62e0ce
     node_instance_profile = regex("^arn:aws:iam::\\d+:role/*.*/(.+)?", var.iam_config.node_role_arn)[0]
     tags                  = var.tags
     dns_cluster_ip        = local.dns_cluster_ip
