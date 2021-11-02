a8b373603df746ddac80b35cca86479f14ad2fda
Ed Robinson <edward-robinson@cookpad.com>
2021-11-02 14:28:12 +0000

    Remove usage of cluster_names in test fixtures

--- examples/cluster/environment/variables.tf
+++ examples/cluster/environment/variables.tf
@@ -7,9 +7,3 @@ variable "cidr_block" {
   type    = string
   default = "10.0.0.0/18"
 }
-
-variable "cluster_names" {
-  description = "Names of the EKS clusters deployed in this VPC."
-  type        = list(string)
-  default     = []
-}
