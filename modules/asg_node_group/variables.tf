82cc786a3a7b6460b696d2042d4608f5d7424adb
Joan Porta <joan-porta@cookpad.com>
2022-05-27 13:58:26 +0200

    Remove the csi-ebs addon because is cleare to have it in a separet PR, to can implement it for lower eks module releases

--- modules/asg_node_group/variables.tf
+++ modules/asg_node_group/variables.tf
@@ -184,9 +184,3 @@ variable "bottlerocket_admin_container_source" {
   default     = ""
   description = "URI of a custom admin container image"
 }
-
-variable "nodes_metadata_http_tokens_optional" {
-  type        = bool
-  default     = false
-  description = "Whether or not the metadata service requires session tokens, also referred to as Instance Metadata Service Version 2 (IMDSv2). Can be optional or required. If using kube2iam use optional"
-}
