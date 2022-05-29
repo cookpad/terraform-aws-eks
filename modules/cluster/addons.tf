b24795d3f218eb70efda4e737e825ef1c9c1a3de
Joan Porta <joan-porta@cookpad.com>
2022-05-29 20:54:44 +0200

    Fix ebs-csi driver version

--- modules/cluster/addons.tf
+++ modules/cluster/addons.tf
@@ -52,7 +52,7 @@ resource "aws_eks_addon" "ebs-csi" {
   count                    = local.aws_ebs_csi_driver_iam_role_count
   cluster_name             = local.config.name
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.6.2-eksbuild.1"
+  addon_version            = "v1.6.1-eksbuild.1"
   service_account_role_arn = aws_iam_role.aws_ebs_csi_driver[0].arn
   resolve_conflicts        = "OVERWRITE"
 }
