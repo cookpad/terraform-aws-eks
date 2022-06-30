7d780960c27a6c5521462c9b5a6d3f003f4ffc40
Ed Robinson <edward-robinson@cookpad.com>
2022-06-30 14:26:25 +0100

    Run tests against latest terraform / aws provider (#321)

--- examples/cluster/environment/backend.tf
+++ examples/cluster/environment/backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "cookpad-terraform-aws-eks-testing"
-    key            = "test-environment"
-    region         = "us-east-1"
-    dynamodb_table = "terraform_locks"
+    bucket = "cookpad-terraform-aws-eks-testing"
+    key    = "test-environment"
+    region = "us-east-1"
   }
 }
