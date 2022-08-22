2ef3a991e790f71dabdb2e6bb3f3c88b526c8ae9
Ed Robinson <edward-robinson@cookpad.com>
2022-08-22 14:41:13 +0100

    Remove cluster local-exec provisioner

--- modules/cluster/kubectl/command.sh
+++ modules/cluster/kubectl/command.sh
@@ -1,5 +1,14 @@
 echo "$KUBECONFIG" > ${kubeconfig_path}
 
+for i in {0..60}
+do if kubectl --kubeconfig=${kubeconfig_path} cluster-info &> /dev/null; then
+  break
+else
+  echo "cluster isn't ready yet"
+  sleep 5
+fi
+done
+
 %{ for r in replace ~}
 echo "$MANIFEST" | kubectl --kubeconfig=${kubeconfig_path} apply -f - || echo "$MANIFEST" | kubectl --kubeconfig=${kubeconfig_path} replace --force --save-config -f -
 %{ endfor ~}
