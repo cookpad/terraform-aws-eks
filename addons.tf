f6f512552d1cdb753d6d134d568da32b49d9b234
Joan Porta <joan-porta@cookpad.com>
2023-06-21 12:55:20 +0200

    Fix 1.25-rc1

--- addons.tf
+++ addons.tf
@@ -3,7 +3,6 @@ resource "aws_eks_addon" "vpc-cni" {
   addon_name                  = "vpc-cni"
   addon_version               = local.versions.vpc_cni
   resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
   configuration_values        = var.vpc_cni_configuration_values
 }
 
@@ -12,7 +11,6 @@ resource "aws_eks_addon" "kube-proxy" {
   addon_name                  = "kube-proxy"
   addon_version               = local.versions.kube_proxy
   resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
   configuration_values        = var.kube_proxy_configuration_values
 }
 
@@ -21,7 +19,6 @@ resource "aws_eks_addon" "coredns" {
   addon_name                  = "coredns"
   addon_version               = local.versions.coredns
   resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
   configuration_values        = var.coredns_configuration_values
   depends_on                  = [aws_eks_fargate_profile.critical_pods]
 }
@@ -32,7 +29,6 @@ resource "aws_eks_addon" "ebs-csi" {
   addon_version               = local.versions.aws_ebs_csi_driver
   service_account_role_arn    = aws_iam_role.aws_ebs_csi_driver.arn
   resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
   configuration_values        = var.ebs_csi_configuration_values
   depends_on                  = [aws_eks_fargate_profile.critical_pods]
 }
