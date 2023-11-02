0d1cb384dca8cf41887599448020f9fcc7b2e01b
Ed Robinson <edward-robinson@cookpad.com>
2023-11-02 16:37:26 +0000

    Split versions.tf file

--- versions.tf
+++ versions.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.47.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.10"
-    }
-  }
-}
-
 # Run hack/versions to generate the latest values for this
 locals {
   versions = {
