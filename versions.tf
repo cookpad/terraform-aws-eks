2056ffc9769d7b381620f9c16c300c93ba2be2f0
Ed Robinson <edward-robinson@cookpad.com>
2023-11-02 16:40:12 +0000

    Update addons to latest versions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,11 @@
-# Run hack/versions to generate the latest values for this
+# Run hack/versions k8sVersionNumber > versions.tf
+# to generate the latest values for this
 locals {
   versions = {
     k8s                = "1.25"
-    vpc_cni            = "v1.12.6-eksbuild.2"
+    vpc_cni            = "v1.16.2-eksbuild.1"
     kube_proxy         = "v1.25.9-eksbuild.1"
-    coredns            = "v1.9.3-eksbuild.3"
-    aws_ebs_csi_driver = "v1.19.0-eksbuild.2"
+    coredns            = "v1.9.3-eksbuild.9"
+    aws_ebs_csi_driver = "v1.27.0-eksbuild.1"
   }
 }
