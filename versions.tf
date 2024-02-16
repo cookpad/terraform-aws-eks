24dcbdb6ab6cae5024ba04e856990cb4a15a6976
Ed Robinson <edward-robinson@cookpad.com>
2024-02-16 15:23:52 +0000

    Update addons to latest versions (#354)
    * Split versions.tf file
    
    * Update hack/versions
    
    So that we don't have to run terraform fmt after updating the versions
    file
    
    * Update addons to latest versions

--- versions.tf
+++ versions.tf
@@ -1,26 +1,11 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.47.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.10"
-    }
-  }
-}
-
-# Run hack/versions to generate the latest values for this
+# Run hack/versions k8sVersionNumber > versions.tf
+# to generate the latest values for this
 locals {
   versions = {
     k8s                = "1.25"
-    vpc_cni            = "v1.12.6-eksbuild.2"
+    vpc_cni            = "v1.16.2-eksbuild.1"
     kube_proxy         = "v1.25.9-eksbuild.1"
-    coredns            = "v1.9.3-eksbuild.3"
-    aws_ebs_csi_driver = "v1.19.0-eksbuild.2"
+    coredns            = "v1.9.3-eksbuild.9"
+    aws_ebs_csi_driver = "v1.27.0-eksbuild.1"
   }
 }
