d216119d9fa0f6d613ba82a9c45f98469d133848
Ed Robinson <edward-robinson@cookpad.com>
2024-07-17 11:54:27 +0100

    Update addons to latest version
    * Fix addon version selection logic
    * Update versions.tf

--- versions.tf
+++ versions.tf
@@ -4,8 +4,8 @@ locals {
   versions = {
     k8s                = "1.27"
     vpc_cni            = "v1.18.2-eksbuild.1"
-    kube_proxy         = "v1.27.8-eksbuild.4"
-    coredns            = "v1.9.3-eksbuild.9"
+    kube_proxy         = "v1.27.12-eksbuild.5"
+    coredns            = "v1.10.1-eksbuild.11"
     aws_ebs_csi_driver = "v1.32.0-eksbuild.1"
   }
 }
