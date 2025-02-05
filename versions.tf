70a90da1066f428a705b66a42266b6e482e818da
Ed Robinson <edward-robinson@cookpad.com>
2025-02-05 13:22:31 +0000

    Upgrade to k8s 1.32 (#378)

--- versions.tf
+++ versions.tf
@@ -2,9 +2,9 @@
 # to generate the latest values for this
 locals {
   versions = {
-    k8s                = "1.31"
+    k8s                = "1.32"
     vpc_cni            = "v1.19.2-eksbuild.1"
-    kube_proxy         = "v1.31.3-eksbuild.2"
+    kube_proxy         = "v1.32.0-eksbuild.2"
     coredns            = "v1.11.4-eksbuild.2"
     aws_ebs_csi_driver = "v1.39.0-eksbuild.1"
   }
