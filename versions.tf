9fab95b3a0ccf48de612a03e8c3440474685bdcc
Ed Robinson <edward-robinson@cookpad.com>
2025-04-17 17:08:11 +0100

    Upgrade addons

--- versions.tf
+++ versions.tf
@@ -3,9 +3,9 @@
 locals {
   versions = {
     k8s                = "1.32"
-    vpc_cni            = "v1.19.2-eksbuild.1"
+    vpc_cni            = "v1.19.3-eksbuild.1"
     kube_proxy         = "v1.32.0-eksbuild.2"
     coredns            = "v1.11.4-eksbuild.2"
-    aws_ebs_csi_driver = "v1.39.0-eksbuild.1"
+    aws_ebs_csi_driver = "v1.41.0-eksbuild.1"
   }
 }
